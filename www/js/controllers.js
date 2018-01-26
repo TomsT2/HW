cb1f0c46ec3be1fd95434c4d81e884b64424c114
Tomaso <tomaso.pastore@live.it>
2018-01-26 12:40:52 +0100

    menu integrato

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -113,8 +113,11 @@ $http({
 
 })
 
-.controller('DashCtrl', function($scope, $state, $http, $ionicPopup, AuthService) {
+.controller('DashCtrl', function($scope, $ionicPopover, $ionicSideMenuDelegate, $state, $http, $ionicPopup, AuthService) {
 
+ $scope.toggleLeft = function() {
+        $ionicSideMenuDelegate.toggleLeft();
+      };
 $scope.servizi = {};
   $scope.logout = function() {
 
@@ -167,5 +170,41 @@ $scope.servizi = {};
         $scope.response = err;
       });
   };
+
+    // .fromTemplate() method
+  var template =
+  '<ion-popover-view><ion-header-bar> <h1 class="title">My Popover Title</h1> </ion-header-bar> <ion-content> Hello! </ion-content></ion-popover-view>';
+
+  $scope.popover = $ionicPopover.fromTemplate(template, {
+    scope: $scope
+  });
+
+//  // .fromTemplateUrl() method
+//  $ionicPopover.fromTemplateUrl('my-popover.html', {
+//    scope: $scope
+//  }).then(function(popover) {
+//    $scope.popover = popover;
+//  });
+
+
+  $scope.openPopover = function($event) {
+    $scope.popover.show($event);
+  };
+  $scope.closePopover = function() {
+    $scope.popover.hide();
+  };
+  //Cleanup the popover when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  });
+  // Execute action on hidden popover
+  $scope.$on('popover.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove popover
+  $scope.$on('popover.removed', function() {
+    // Execute action
+  });
+
 });
 
